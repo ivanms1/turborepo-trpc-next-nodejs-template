b551d1ee2b902187b21efaac2f9bba3b62af409f
Ivan Saldano <ivanms1@gmail.com>
2023-01-05 16:19:35 +0700

    feat: setup tRPC and packages

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,15 @@
 import { Button } from "ui";
 
+import { trpc } from "@/utils/trpc";
+
 export default function Web() {
+  const jobPostsQuery = trpc.jobPost.all.useQuery();
   return (
     <div>
       <h1>Web</h1>
+      {jobPostsQuery?.data?.map((jobPost) => (
+        <div key={jobPost.id}>{jobPost.role}</div>
+      ))}
       <Button />
     </div>
   );
